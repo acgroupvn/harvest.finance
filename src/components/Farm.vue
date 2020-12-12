787ff5dc05f3f09bc7c4f7fb6f8397f84e3df8ba
lai_phuc <laiphuc2310@gmail.com>
2020-12-12 11:14:25 +0700

    update

---
+++ src/components/Farm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+        <h1>day la the h1</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
