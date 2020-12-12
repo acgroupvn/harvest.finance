4b40e60672168518539a07ad1d2ef9d6f61b2a50
nguyet <binguyet.12@example.com>
2020-12-11 17:56:01 -0800

    a

---
+++ src/App.vue
@@ -0,0 +1,174 @@
+<template>
+  <div id="app">
+     <div class="harvest__header">
+        <span class="harvest__header-content">
+          🧑‍🌾 Week 15 reward updates are live! 👩‍🌾
+          <a href="">Details</a>
+        </span>
+     </div>
+     <div class="background"> 
+     <div class="background1">
+     <div class="harvest__body">
+        <div class="harvest__body-header">
+          <div class="harvest__body-header-xeHavest">
+            <img src="../src/assets/img/xe.gif" alt="">
+            "Harvest"
+          </div>
+          <div class="harvest__body-header-menu">
+            <a href="">Farm</a>
+            <a href="">Stake</a>
+            <a href="">FAQ</a>
+            <a href="">Dashboard</a>
+            <a href="">Stats</a>
+            <a href="">Wiki</a>
+            <a href="">策略</a>
+            <a href="">Claim</a>
+          </div>
+        </div>
+     </div>
+     <div class="harvest__footer">
+
+     </div>
+     </div>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.harvest__header{
+    width: auto;
+    font-size: 14px;
+    display: flex;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    align-items: center;
+    border-width: 2px 0px;
+    border-top-style: solid;
+    border-bottom-style: solid;
+    border-top-color: black;
+    border-bottom-color: black;
+    border-image: initial;
+    border-right-style: initial;
+    border-right-color: initial;
+    border-left-style: initial;
+    border-left-color: initial;
+    background-color: rgb(255, 252, 230);
+    padding: 10px;
+    margin-top:-3.5%;
+}
+.harvest__header-content{
+   font-weight: bold;
+   margin-right:10px;
+}
+.harvest__header-content a{
+  margin-right:5px;
+  margin-left:5px;
+  color:black;
+  font-weight:bold;
+}
+.background{
+  width: 100%;
+  z-index: -1;
+  position:absolute;
+  height: 1754px;
+  background-size: cover;
+  background-image: url('../src/assets/img/background1.jpg');
+}
+.background1{
+  width: 100%;
+  z-index: -1;
+  position:absolute;
+  height: 1754px;
+  background-size: cover;
+  background-image: url('../src/assets/img/background2.png');
+}
+.harvest__body-header-xeHavest{
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    position: absolute;
+    left: 10px;
+    font-weight: bold;
+    font-size: 24px;
+    top: 3px;
+}
+.harvest__body-header-xeHavest img{
+width:50px;
+margin-right:10px;
+border:1px solid black;
+border-radius:2px;
+}
+.harvest__body-header{
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 900px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    background-image: url(../src/assets/img/navbar-divider.svg);
+    background-position: center bottom;
+    background-repeat: no-repeat;
+    height: 60px;
+    overflow: overlay;
+}
+ 
+.harvest__body-header-menu{
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    grid-column: 2 / 2;
+}
+.harvest__body-header-menu a{
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    color: black;
+    opacity: 0.5;
+    font-weight: bold;
+    margin-right: 20px;
+    width: 80px;
+    height: 52px;
+    text-decoration: none;
+}
+</style>
---
+++ src/assets/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="harvest__background-img">
+    <div class="harvest__header">
+    
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .harvest__background-img{
+
+    }
+</style>
